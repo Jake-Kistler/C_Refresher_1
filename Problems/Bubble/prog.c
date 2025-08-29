ec6964572246bcdff564f24e05f41bb50a9da91a
Jake <jakistler01@gmail.com>
2025-08-29 11:29:17 -0500

    fixed bubble sort to make use of testing in makefile

--- Problems/Bubble/prog.c
+++ Problems/Bubble/prog.c
@@ -1,51 +1,42 @@
-// Bubble sort in C using a Makefile for PDN 
-// Jake Kistler 
-// Universty of Oklahoma 
+// Bubble sort in C using a Makefile for PDN
+// Jake Kistler
+// University of Oklahoma
 
 /*
-At a high level this is rather simple. 
-step 1: Read in input file name, output file name 
-step 2: Read in integers from input file with the first number in the file being the total number of numbers that exist in the file
-step 3: Sort the integers using bubble sort
-step 4: Write the sorted integers to the output file with the first number again being the total numbers in the file
+At a high level this is rather simple.
+step 1: Read in input file name, output file name
+step 2: Read in floats from input file with the first number in the file being the total number of numbers that exist in the file
+step 3: Sort the floats using bubble sort
+step 4: Write the sorted floats to the output file with the first number again being the total numbers in the file
 step 5: Free and terminate
 */
 
-#include <stdio.h>  // file IO
-#include <stdlib.h> // malloc and free come from here
+#include <stdio.h>   // file IO
+#include <stdlib.h>  // malloc and free
+#include <errno.h>   // perror
 
 // function prototypes
 
 // Helper functions
 void print_array(float *arr, int size);
-// Core function
-void bubble_sort(float *arr, int size);
 
-int main(int argc, char *argv[]) 
+// Core function
+static void bubble_sort(float *arr, int size);
+
+int main(int argc, char *argv[])
 {
     // Step 1: Read in input file name, output file name
-    // checks for 2 file names and if they are not present, exits program
-    /*
-    in cpp this looks like:
-    if(argc <3)
-    {
-        std::cerr << "Usage: " << argv[0] << " <input.txt> <output.txt>" << std::endl;
-        return 1;
-    //}
-    */
-
     if (argc < 3)
     {
         fprintf(stderr, "Usage: %s <input.txt> <output.txt>\n", argv[0]);
         return 1;
     }
 
-    // input and output file paths as constants so we don't break them
     const char *input_file_path = argv[1];
     const char *output_file_path = argv[2];
 
-    // step 2: (Try) to read in integers from input file with the first number in the file being the total number of numbers that exist in the file
-    FILE *input = fopen(input_file_path, "r"); // open in read mode
+    // Step 2: (Try) to read in floats from input file with the first number as the total count
+    FILE *input = fopen(input_file_path, "r");
     if (!input)
     {
         perror("Error opening input file");
@@ -53,29 +44,25 @@ int main(int argc, char *argv[])
     }
 
     int size_of_file = 0;
-    if (fscanf(input, "%d", &size_of_file) != 1 || size_of_file < 0) // checks for one int being read and guards aginst negive counts being read
+    if (fscanf(input, "%d", &size_of_file) != 1 || size_of_file < 0)
     {
         fprintf(stderr, "Invalid count at start of the input file.\n");
         fclose(input);
         return 1;
     }
 
-    // Step 3: using malloc create the array that we will be sorting
     float *arr = NULL;
     if (size_of_file > 0)
     {
-        // set the pointer arr (of type float) to the starting address of the memory block that will be big enough (hopefully) to hold the contents of the input file
         arr = (float *)malloc((size_t)size_of_file * sizeof(float));
-
         if (!arr)
         {
-            fprintf(stderr, "Malloc failed for %d floats!\n" , size_of_file);
+            fprintf(stderr, "Malloc failed for %d floats!\n", size_of_file);
             fclose(input);
             return 1;
         }
 
-        // now time to read the file into the array
-        for (unsigned int i = 0; i < size_of_file; i++)
+        for (int i = 0; i < size_of_file; i++)
         {
             if (fscanf(input, "%f", &arr[i]) != 1)
             {
@@ -85,36 +72,34 @@ int main(int argc, char *argv[])
                 return 1;
             }
         }
+    }
+    fclose(input);
 
-        // now that we have read the input file we will close it
-        fclose(input);
-
-        //call  BUBBLE_SORT
+    // Step 3: bubble sort
+    if (size_of_file > 1)
+    {
         bubble_sort(arr, size_of_file);
-
-        // handle output file
-        FILE *output = fopen(output_file_path, "w"); // open in write mode
-
-        if(!output)
-        {
-            perror("Error opening the ouput file!\n");
-            free(arr);
-            return 1;
-        }
-
-        // print the first line (the total number of floats) to the file
-        fprintf(output, "%d\n", size_of_file);
-
-        // print the sorted array on a new line 
-        for (unsigned int i = 0; i < size_of_file; i++)
-        {
-            fprintf(output, "%f\n", arr[i]);
-        }
-
-        fclose(output); 
-        free(arr);
     }
 
+    // Step 4: write output
+    FILE *output = fopen(output_file_path, "w");
+    if (!output)
+    {
+        perror("Error opening the output file");
+        free(arr);
+        return 1;
+    }
+
+    fprintf(output, "%d\n", size_of_file);
+    for (int i = 0; i < size_of_file; i++)
+    {
+        fprintf(output, "%f\n", arr[i]);
+    }
+
+    fclose(output);
+    free(arr);
+
+    // Step 5: terminate
     return 0;
 }
 
@@ -124,25 +109,30 @@ int main(int argc, char *argv[])
 
 void print_array(float *arr, int size)
 {
-    for (unsigned int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
         printf("%f\n", arr[i]);
     }
 }
 
-// bubble sort is a O(n^2) sorting algorithm that 
-void bubble_sort(float *arr, int size)
+static void bubble_sort(float *arr, int size)
 {
-    for (unsigned int i = 0; i < size; i++)
+    for (int i = 0; i < size - 1; i++)
     {
-        for (unsigned int j = 0; j < size - i - 1; j++)
+        int swapped = 0;
+        for (int j = 0; j < size - 1 - i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
-                float temp = arr[j];
+                float t = arr[j];
                 arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                arr[j + 1] = t;
+                swapped = 1;
             }
+        }
+        if (!swapped)
+        {
+            break;
         }
     }
 }
